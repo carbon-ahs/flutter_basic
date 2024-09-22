519e44139cbf38c5b358035d22b7b57cf43a9e08
Ahsan Habib <87078868+carbon-ahs@users.noreply.github.com>
2024-09-22 16:24:33 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:fitness/pages/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        fontFamily: 'Poppins',
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
